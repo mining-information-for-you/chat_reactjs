5123c493b06b172a7acbc477d25044f1e1c971a2
Bruno Martins <brunomartins@MacBook-Pro-de-Bruno.local>
2018-07-15 22:02:22 -0300

    conf scss

---
+++ src/components/Principal/Principal.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import './principal.scss'
+
+class Principal extends Component {
+
+      render(){
+
+        return(
+          <div  className="teste" id="frame">
+            <h1>Teste</h1>
+
+          </div>
+        )
+      }
+}
+
+
+export default Principal;
